728837fec426dede0c7f669a11324b8d135e17e7
Yauheni Savitski <yauheni.savitski@ext.conrad.com>
2023-06-19 13:27:31 +0200

    Support extra config out of the box

--- src/test/java/io/dropwizard/flyway/FlywayFactoryTest.java
+++ src/test/java/io/dropwizard/flyway/FlywayFactoryTest.java
@@ -1,6 +1,9 @@
 package io.dropwizard.flyway;
 
+import com.google.common.collect.ImmutableMap;
+import jakarta.validation.constraints.NotNull;
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.internal.database.postgresql.PostgreSQLConfigurationExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -8,10 +11,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.sql.DataSource;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class FlywayFactoryTest {
@@ -29,5 +31,29 @@ public class FlywayFactoryTest {
         assertEquals(StandardCharsets.UTF_8, flyway.getConfiguration().getEncoding());
         assertEquals("flyway_schema_history", flyway.getConfiguration().getTable());
         assertEquals(0, flyway.getConfiguration().getSchemas().length);
+    }
+
+    @Test
+    public void checkConfigurationWithOverridedSetting() {
+        Flyway flyway = new FlywayFactory().build(mockDataSource);
+        assertNotNull(flyway);
+
+        boolean transactionalLockDefault = getPostgresTransactionLockSettings(flyway);
+        boolean transactionalLockOverrided = !transactionalLockDefault;
+
+        final FlywayFactory factory = new FlywayFactory();
+        Map<String, String> configuration = ImmutableMap.of("flyway.postgresql.transactional.lock", String.valueOf(transactionalLockOverrided));
+        factory.setConfiguration(configuration);
+        flyway =  factory.build(mockDataSource);
+
+        assertNotNull(flyway);
+        assertEquals(StandardCharsets.UTF_8, flyway.getConfiguration().getEncoding());          // default value
+        assertEquals(0, flyway.getConfiguration().getSchemas().length);                 // default value
+        assertEquals("flyway_schema_history", flyway.getConfiguration().getTable());    // default value
+        assertEquals(transactionalLockOverrided, getPostgresTransactionLockSettings(flyway));   // overrided value
+    }
+
+    private static boolean getPostgresTransactionLockSettings(@NotNull final Flyway flyway) {
+        return flyway.getConfiguration().getPluginRegister().getPlugin(PostgreSQLConfigurationExtension.class).isTransactionalLock();
     }
 }
